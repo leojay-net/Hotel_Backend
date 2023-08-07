f46eda88784832a386b0f3af1bc0bc78f60a1af5
leojay-net <102752992+leojay-net@users.noreply.github.com>
2023-08-07 15:54:15 +0100

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -3,15 +3,11 @@ python -m pip install --upgrade pip
 
 pip install -r requirements.txt
 
-pip freeze > requirements.txt
-
 python manage.py makemigrations
 
-python manage.py collectstatic
+python manage.py collectstatic --no-input
 
 python manage.py migrate
-
-python manage.py collectstatic
 
 python manage.py spectacular --file schema.yml
 
